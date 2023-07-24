e6d32b4e7c812a2c92424bec94da9203b4f98da1
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-07-24 13:03:44 -0600

    Using fetch API to connect with the server

--- service/client-service.js
+++ service/client-service.js
@@ -39,29 +39,35 @@ const table = document.querySelector("[data-table]");
 // Update - PUT/PATCH
 // Delete - DELETE
 
-const listaClientes = () => {
-    //Funcion async
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-        http.open("GET", "http://localhost:3000/perfil");
 
-        //Manda la información
-        http.send();
+//Fetch API - ya viene activa en el navegador
+//Regresa una promesa
+//Transformar la respuesta a formato JSON
 
-        //Recibe la información de db.json
-        http.onload = () => {
-            //Convierte el texto de data a formato JS
-            const response = JSON.parse(http.response);
-            //Verificar status de la llamada http
-            if (http.status >= 400) {
-                reject(response);
-            } else {
-                resolve(response);
-            }
-        }
-    });
-    return promise
-};
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+//---------------Ambas hacen lo mismo---------------
+// //Funcion async
+// const promise = new Promise((resolve, reject) => {
+//     const http = new XMLHttpRequest();
+//     http.open("GET", "http://localhost:3000/perfil");
+
+//     //Manda la información
+//     http.send();
+
+//     //Recibe la información de db.json
+//     http.onload = () => {
+//         //Convierte el texto de data a formato JS
+//         const response = JSON.parse(http.response);
+//         //Verificar status de la llamada http
+//         if (http.status >= 400) {
+//             reject(response);
+//         } else {
+//             resolve(response);
+//         }
+//     }
+// });
+// return promise
 
 //Data es la informacion del archivo (response)
 listaClientes().then((data) => {
